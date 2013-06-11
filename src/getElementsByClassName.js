2c6373a207b40c705b8c75313117a684dc4836c4
Hao Liu <haoliu119@gmail.com>
2013-06-11 19:38:31 -0400

    getElementsByClassName recursive function completed, specrun passed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,27 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var result=[],
+  	  getElementByClass = function (nodes){
+  	  	if (nodes!==undefined){
+  	  		var classList=nodes.classList;
+	  	  	if (nodes.length){
+	  	  		for (var i=0, len=nodes.length; i<len;i++){
+	  	  			if (nodes[i].classList!==undefined){
+	  	  				getElementByClass(nodes[i]);
+	  	  			}
+	  	  		}
+	  	  	}
+	  	  	else {
+	  	  		if (classList){
+		  	  		if (classList.contains(className)) {
+			  	  		result.push(nodes);
+			  	  	}
+		  	  		getElementByClass(nodes.childNodes); 		  	  			
+	  	  		}
+	  	  	}
+	  	}
+  };
+  getElementByClass(document.body);
+  return result;
 };
